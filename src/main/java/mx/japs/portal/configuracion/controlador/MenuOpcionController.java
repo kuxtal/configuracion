777a191f1118efd77d25f975822145af5187678f
Kuxtal <japs.8205@gmail.com>
2015-10-12 12:38:20 -0500

    Agregar funcionalidad de desplegar datosd de menus

--- src/main/java/mx/japs/portal/configuracion/controlador/MenuOpcionController.java
+++ src/main/java/mx/japs/portal/configuracion/controlador/MenuOpcionController.java
@@ -2,7 +2,18 @@ package mx.japs.portal.configuracion.controlador;
 import mx.japs.portal.configuracion.modelo.MenuOpcion;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 
 @RequestMapping("/menuopcions")
@@ -10,4 +21,16 @@ import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 @RooWebScaffold(path = "menuopcions", formBackingObject = MenuOpcion.class)
 @RooWebJson(jsonObject = MenuOpcion.class)
 public class MenuOpcionController {
+	@RequestMapping(value = "/{id}/menuopciones", method = RequestMethod.GET, headers = "Accept=application/json")
+	@ResponseBody
+	public ResponseEntity<String> menuOpcionesJson(@PathVariable("id") Long idMenuOpcion, @RequestParam Map<String,String> jsonParams) {
+		HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        try {
+            List<MenuOpcion> result = menuOpcionRepository.findByOpcionPadre_Id(idMenuOpcion);
+            return new ResponseEntity<String>(MenuOpcion.toJsonArray(result), headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+	}
 }
--- src/main/java/mx/japs/portal/configuracion/repositorio/MenuOpcionRepository.java
+++ src/main/java/mx/japs/portal/configuracion/repositorio/MenuOpcionRepository.java
@@ -1,7 +1,13 @@
 package mx.japs.portal.configuracion.repositorio;
 import mx.japs.portal.configuracion.modelo.MenuOpcion;
+
+import java.util.List;
+
 import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
 
 @RooJpaRepository(domainType = MenuOpcion.class)
 public interface MenuOpcionRepository {
+	public List<MenuOpcion> findByModulo_Id(Long idModulo);
+	
+	public List<MenuOpcion> findByOpcionPadre_Id(Long idOpcionPadre);
 }
